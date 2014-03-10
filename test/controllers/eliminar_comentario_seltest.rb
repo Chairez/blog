14ce3ff4215c5ecd90c91877a92f598cc318b3ea
HelttonRamirez <conker_2_2@hotmail.com>
2014-03-10 16:26:02 -0600

    eliminar comentario...

--- test/controllers/eliminar_comentario_seltest.rb
+++ test/controllers/eliminar_comentario_seltest.rb
@@ -4,7 +4,7 @@ require "json"
 require "selenium-webdriver"
 require "test/unit"
 
-class EliminarComentarioSeltest < Test::Unit::TestCase
+class CrearComentarioSeltest < Test::Unit::TestCase
 
   def setup
     @driver = Selenium::WebDriver.for :phantomjs
@@ -12,27 +12,27 @@ class EliminarComentarioSeltest < Test::Unit::TestCase
     @accept_next_alert = true
     @driver.manage.timeouts.implicit_wait = 30
     @verification_errors = []
+  end
   
   def teardown
     @driver.quit
     assert_equal [], @verification_errors
   end
   
-  def test_eliminar_comentario_sel
+  def test_crear_comentario_sel
     @driver.get(@base_url)
     @driver.find_element(:link, "Crear artículo").click
     @driver.find_element(:id, "post_titulo").clear
-    @driver.find_element(:id, "post_titulo").send_keys "articulo 1"
+    @driver.find_element(:id, "post_titulo").send_keys "prueba 1"
     @driver.find_element(:id, "post_texto").clear
-    @driver.find_element(:id, "post_texto").send_keys "texto del articulo 1"
+    @driver.find_element(:id, "post_texto").send_keys "articulo de prueba 1"
     @driver.find_element(:name, "commit").click
     @driver.find_element(:id, "comment_commenter").clear
-    @driver.find_element(:id, "comment_commenter").send_keys "usuario n"
+    @driver.find_element(:id, "comment_commenter").send_keys "usuario de prueba"
     @driver.find_element(:id, "comment_body").clear
-    @driver.find_element(:id, "comment_body").send_keys "comentario n"
+    @driver.find_element(:id, "comment_body").send_keys "comentario de prueba"
     @driver.find_element(:name, "commit").click
     @driver.find_element(:link, "Eliminar Comentario").click
-    assert_match /^¿Estás seguro[\s\S]$/, close_alert_and_get_its_text()
     assert !element_present?(:link, "Eliminar Comentario")
   end
   
