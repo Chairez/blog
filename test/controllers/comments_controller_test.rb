ee58e4cba1a0984f67e9b83d6bff31fc62e6c0ae
jesusm19 <jesus_heavy19@hotmail.com>
2014-03-11 02:26:48 -0600

    unit test comments Fail

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,8 +1,31 @@
+# encoding: UTF-8
+require 'simplecov'
+SimpleCov.start
 require 'test_helper'
 
+# Clase de pruebas de unidad: Comments
 class CommentsControllerTest < ActionController::TestCase
+ def setup
+    @post = posts(:one)
+  end
+  test "the truth" do
+    assert true   
+  end
+  # Pruebas de creacion de comentario:
+  #test 'debe crear un comentario' do
+   # get :new
+    #assert_response :success
+  #end
 
-	def setup
-		@post=posts(:one)
-	end
+  test 'no guardar un comentario si no tiene texto' do
+    post = Post.new
+    assert !post.save
+  end
+
+  # Pruebas para eliminar un comentario
+  #test 'debe destruir un comentario' do
+   # assert_difference('Post.count', -1) do
+    #  delete :destroy, id: @post.id
+    #end
+  #end
 end
